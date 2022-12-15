f8bedec0ff993dd57b23a25e271dcb9c548b2390
Joel Lozano <lozano.joel.a@gmail.com>
2022-12-14 19:28:37 -0500

    Updates

---
+++ precache-manifest.22c1ec03cb2ec653c114291a8c7001f6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "f750f47c7e3a424d13b0",
+    "url": "/todo-list/static/css/main.2d4f7e72.chunk.css"
+  },
+  {
+    "revision": "f750f47c7e3a424d13b0",
+    "url": "/todo-list/static/js/main.9f63a2f2.chunk.js"
+  },
+  {
+    "revision": "ca797cc406ded84cf733",
+    "url": "/todo-list/static/js/runtime~main.41b69a58.js"
+  },
+  {
+    "revision": "0bb63192aa8f45fa7734",
+    "url": "/todo-list/static/js/2.e90cff1c.chunk.js"
+  },
+  {
+    "revision": "2582aef8d4679a898e057e209b9c0f18",
+    "url": "/todo-list/static/media/check_icon.2582aef8.jpg"
+  },
+  {
+    "revision": "9177df396ccbbf02eaa631b78f3471a7",
+    "url": "/todo-list/static/media/trash_icon.9177df39.jpg"
+  },
+  {
+    "revision": "e3663eccccfb76dea035a9acef13cd33",
+    "url": "/todo-list/index.html"
+  }
+];
